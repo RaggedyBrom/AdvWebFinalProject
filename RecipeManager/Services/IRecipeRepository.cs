0f381342a61d9b641262e2f34795752bed580ecc
Jeremy <brom.eeyhib@gmail.com>
2022-11-06 17:35:00 -0500

    Create interface for Recipe repository

---
+++ RecipeManager/Services/IRecipeRepository.cs
@@ -0,0 +1,20 @@
+﻿using RecipeManager.Models.Entities;
+
+namespace RecipeManager.Services
+{
+    /// <summary>
+    /// Defines methods required for a repository implementation of the Recipe entity.
+    /// </summary>
+    public interface IRecipeRepository
+    {
+        public Task<ICollection<Recipe>> ReadAllAsync();
+
+        public Task<Recipe?> ReadAsync(int recipeId);
+
+        public Task<bool> CreateAsync(Recipe recipe);
+
+        public Task<bool> UpdateAsync(int recipeId, Recipe recipe);
+
+        public Task<bool> DeleteAsync(int recipeId);
+    }
+}
