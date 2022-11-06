99b70e4c4013713e3704c89e844ef6915667216c
Jeremy <brom.eeyhib@gmail.com>
2022-11-06 16:01:40 -0500

    Create the database context

---
+++ RecipeManager/Services/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeManager.Models.Entities;
+
+namespace RecipeManager.Services
+{
+    /// <summary>
+    /// Serves as a context between the application and the database, with DbSet
+    /// properties corresponding to entities in the database and models in the
+    /// application.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        /// <summary>
+        /// Default constructor which takes in options and passes them to the base class.
+        /// </summary>
+        /// <param name="options">Options to apply to this application's implementation of the DbContext.</param>
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+        public DbSet<Recipe> Recipe => Set<Recipe>();
+        public DbSet<RecipeIngredient> RecipeIngredients => Set<RecipeIngredient>();
+    }
+}
