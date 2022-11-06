5ef69bf76feb1ed4a5e656018fc01a5b3c869105
Jeremy <brom.eeyhib@gmail.com>
2022-11-06 15:38:23 -0500

    Create a model for ingredients

---
+++ RecipeManager/Models/Entities/Ingredient.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeManager.Models.Entities
+{
+    /// <summary>
+    /// Models an ingredient.
+    /// </summary>
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        [MaxLength(64)]
+        public string Name { get; set; } = string.Empty;
+        public FoodGroup? FoodGroup { get; set; }
+    }
+
+    public enum FoodGroup
+    {
+        Fruit,
+        Vegetable,
+        Grain,
+        Protein,
+        Dairy
+    }
+}
