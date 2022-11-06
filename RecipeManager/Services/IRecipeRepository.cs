252b89e562cdbbac6ff35f09ab48fea48d3c737a
Jeremy <brom.eeyhib@gmail.com>
2022-11-06 17:43:52 -0500

    Add documentation

--- RecipeManager/Services/IRecipeRepository.cs
+++ RecipeManager/Services/IRecipeRepository.cs
@@ -7,14 +7,40 @@ namespace RecipeManager.Services
     /// </summary>
     public interface IRecipeRepository
     {
+
+        /// <summary>
+        /// Reads all recipes from the database.
+        /// </summary>
+        /// <returns>A collection of recipes from the database.</returns>
         public Task<ICollection<Recipe>> ReadAllAsync();
 
+        /// <summary>
+        /// Reads a single recipe from the database.
+        /// </summary>
+        /// <param name="recipeId">The Id of the desired recipe.</param>
+        /// <returns>A reference to desired recipe, or null if it was not found.</returns>
         public Task<Recipe?> ReadAsync(int recipeId);
 
+        /// <summary>
+        /// Creates a new recipe in the database.
+        /// </summary>
+        /// <param name="recipe">A recipe to be added to the database.</param>
+        /// <returns>A boolean incidicating whether or not the operation was successful.</returns>
         public Task<bool> CreateAsync(Recipe recipe);
 
-        public Task<bool> UpdateAsync(int recipeId, Recipe recipe);
+        /// <summary>
+        /// Updates an existing recipe in the database.
+        /// </summary>
+        /// <param name="recipeId">The Id of the recipe to be altered.</param>
+        /// <param name="recipe">A recipe object which will be used to update the existing database entity.</param>
+        /// <returns>A reference to the recipe that was updated, or null if the operation was unsuccessful.</returns>
+        public Task<Recipe?> UpdateAsync(int recipeId, Recipe recipe);
 
+        /// <summary>
+        /// Deletes a recipe from the database.
+        /// </summary>
+        /// <param name="recipeId">The Id of the recipe to be deleted.</param>
+        /// <returns>A boolean incidicating whether or not the operation was successful.</returns>
         public Task<bool> DeleteAsync(int recipeId);
     }
 }
