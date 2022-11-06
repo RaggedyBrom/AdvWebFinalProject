5e228cc75724be6a94572bbe5b1117591e89f9aa
Jeremy <brom.eeyhib@gmail.com>
2022-11-06 15:30:14 -0500

    Create model for recipes

---
+++ RecipeManager/Models/Entities/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeManager.Models.Entities
+{
+    /// <summary>
+    /// Models a recipe.
+    /// </summary>
+    public class Recipe
+    {
+        public int Id { get; set; }
+        [MaxLength(64)]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int PrepTime { get; set; }
+        public int CookTime { get; set; }
+    }
+}
