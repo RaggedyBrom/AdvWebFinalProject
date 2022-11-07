85baff280d2e25e3abb609a9ec13ee0283a2739b
Jeremy <brom.eeyhib@gmail.com>
2022-11-07 13:18:30 -0500

    Create POST endpoint for recipe API

--- RecipeManager/Controllers/API/RecipeController.cs
+++ RecipeManager/Controllers/API/RecipeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using RecipeManager.Models.Entities;
 using RecipeManager.Services;
+using System.Net;
 
 namespace RecipeManager.Controllers.API
 {
@@ -27,7 +29,7 @@ namespace RecipeManager.Controllers.API
         /// This action method returns a collection of all recipes found by the repo, formatted as JSON.
         /// </summary>
         /// <returns>A task indicating the result of the action method.</returns>
-        [HttpGet("all")]
+        [HttpGet]
         public async Task<IActionResult> Get()
         {
             var recipes = await _recipeRepo.ReadAllAsync();
@@ -39,11 +41,28 @@ namespace RecipeManager.Controllers.API
         /// </summary>
         /// <param name="id">The Id of the recipe to be returned.</param>
         /// <returns>A task indicating the result of the action method.</returns>
-        [HttpGet("one/{id}")]
+        [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
             var recipe = await _recipeRepo.ReadAsync(id);
             return Ok(recipe);
+        }
+
+        /// <summary>
+        /// This action method takes in a recipe and uses the repo to store it to the database.
+        /// </summary>
+        /// <param name="recipe">A recipe object created from HTTP form data.</param>
+        /// <returns>A task indicating the result of the action method.</returns>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm]Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                await _recipeRepo.CreateAsync(recipe);
+                return Ok();
+            }
+            return BadRequest();
+                
         }
     }
 }
