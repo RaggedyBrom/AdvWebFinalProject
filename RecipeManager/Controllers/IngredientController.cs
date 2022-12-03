adf9f15b94f48b52f0fc74a7f45d22c2c8e2d192
Jeremy <brom.eeyhib@gmail.com>
2022-12-03 05:40:05 -0500

    Create controllers for displaying recipes and ingredients

---
+++ RecipeManager/Controllers/IngredientController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeManager.Models.ViewModels;
+using RecipeManager.Services;
+
+namespace RecipeManager.Controllers
+{
+    /// <summary>
+    /// Controller which returns views relevant to ingredients.
+    /// </summary>
+    public class IngredientController : Controller
+    {
+        private readonly IIngredientRepository _ingredientRepo;
+
+        /// <summary>
+        /// Default constructor which injects an instance of the ingredient
+        /// repository.
+        /// </summary>
+        /// <param name="ingredientRepo">An instance of the ingredient repository
+        /// used to query the database.</param>
+        public IngredientController(IIngredientRepository ingredientRepo)
+        {
+            _ingredientRepo = ingredientRepo;
+        }
+
+        /// <summary>
+        /// Reads all ingredients from the repository, converts them to view models,
+        /// and returns a view displaying a list of the ingredients.
+        /// </summary>
+        /// <returns>A view that displays a list of IngredientVMs.</returns>
+        public async Task<IActionResult> Index()
+        {
+            var ingredients = await _ingredientRepo.ReadAllAsync();
+            var model = ingredients.Select(i => IngredientVM.GetIngredientVM(i));
+            return View(model);
+        }
+    }
+}
---
+++ RecipeManager/Controllers/RecipeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeManager.Models.ViewModels;
+using RecipeManager.Services;
+
+namespace RecipeManager.Controllers
+{
+    /// <summary>
+    /// Controller which returns views relevant to recipes.
+    /// </summary>
+    public class RecipeController : Controller
+    {
+        private readonly IRecipeRepository _recipeRepo;
+
+        /// <summary>
+        /// Default contstructor which injects an instance of the
+        /// recipe repository.
+        /// </summary>
+        /// <param name="recipeRepo">An instance of the recipe repository
+        /// to be used in database queries.</param>
+        public RecipeController(IRecipeRepository recipeRepo)
+        {
+            _recipeRepo = recipeRepo;
+        }
+
+        /// <summary>
+        /// Reads all the recipes from the repository, converts them to
+        /// view models, and returns a view displaying a list of the
+        /// recipes.
+        /// </summary>
+        /// <returns>A view that displays a list of RecipeVMs.</returns>
+        public async Task<IActionResult> Index()
+        {
+            var recipes = await _recipeRepo.ReadAllAsync();
+            var model = recipes.Select(r => RecipeVM.GetRecipeVM(r));
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var recipe = await _recipeRepo.ReadAsync(id);
+
+            if (recipe != null)
+            {
+                var model = RecipeVM.GetRecipeVM(recipe);
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
